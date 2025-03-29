439c10c4e69c6c9e5afffc7c4e108d617dd2b75a
alexmakijokela <alex.makijokela@gmail.com>
2025-03-29 15:08:22 -0700

    patch: add emotions list and details

--- flask_app/luma_video_maker.py
+++ flask_app/luma_video_maker.py
@@ -1,9 +1,12 @@
 import os
 import time
+import random
 from lumaai import LumaAI
+from emotions_list import emotions_list
 
-emotion1="Sadness"
-emotion2="Determination"
+
+emotion1=random.choice(emotions_list)
+emotion2=random.choice(emotions_list)
 
 def wait_until_generation_finishes(client, start_generation):
     completed = False
@@ -12,7 +15,8 @@ def wait_until_generation_finishes(client, start_generation):
       if generation.state == "completed":
         completed = True
       elif generation.state == "failed":
-        raise RuntimeError(f"Generation failed: {start_generation.failure_reason}")
+        print(generation)
+        raise RuntimeError(f"Generation failed: {generation.failure_reason}")
       print("Dream state:", generation.state)
       time.sleep(2)
     return generation.assets.image
@@ -21,13 +25,14 @@ def wait_until_generation_finishes(client, start_generation):
 client = LumaAI(auth_token=os.getenv("LUMAAI_API_KEY"))
 
 # Reference image URL and prompts for start and end keyframes
-reference_image_url = "https://www.michaeldivine.com/wp-content/uploads/2021/01/Station-to-Station-1.jpg"
-reference_image_weight= 0.75
+reference_image_url_start = "https://www.michaeldivine.com/wp-content/uploads/2021/01/Apsara5-2.jpg"
+reference_image_url_end = "https://www.michaeldivine.com/wp-content/uploads/2021/01/Station-to-Station-1.jpg"
+reference_image_weight= 0.95
 start_prompt = f"draw a semi-abstract scene depicting profound {emotion1}"
 end_prompt = f"draw a semi-abstract scene depicting profound {emotion2}"
 
 image_ref_object = [{
-  "url": reference_image_url,
+  "url": reference_image_url_start,
   "weight": reference_image_weight
 }]
 
@@ -35,8 +40,16 @@ image_ref_object = [{
 start_generation = client.generations.image.create(prompt=start_prompt, image_ref=image_ref_object)
 start_keyframe_url = wait_until_generation_finishes(client, start_generation)
 
+last_keyframe_image_ref_based_on_first_keyframe= [{
+  "url": reference_image_url_end,
+  "weight": 0.75
+},{"url": start_keyframe_url,
+   "weight": 0.25}]
+
+transform_first_keyframe_prompt = f"transform the reference image into a scene depicting profound {emotion2}"
+
 # Generate end keyframe
-end_generation = client.generations.image.create(prompt=end_prompt, image_ref=image_ref_object)
+end_generation = client.generations.image.create(prompt=transform_first_keyframe_prompt, image_ref=last_keyframe_image_ref_based_on_first_keyframe)
 end_keyframe_url = wait_until_generation_finishes(client, end_generation)
 
 # Define the start and end keyframes and prompt
@@ -70,7 +83,7 @@ while not completed:
   time.sleep(3)
 
 # Print the URL to view the video
-print(f"Reference image url: {reference_image_url}, weight {reference_image_weight}")
+print(f"Reference image url: {reference_image_url_start}, weight {reference_image_weight}")
 print(f"Emotional transition: {emotion1} to {emotion2}")
 print(f"Start keyrame prompt: {start_prompt}")
 print(f"End keyrame prompt: {end_prompt}")
